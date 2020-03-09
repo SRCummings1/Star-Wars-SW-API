417751ea1b88542d2884a0f1b60676b97b5ed44c
Sean Cummings <srcummings85@gmail.com>
2020-03-09 11:50:55 -0400

    fourth commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SWAPI.Models;
+
+namespace SW_API.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public async Task<IActionResult> Index()
+
+        {
+            return View();
+        }
+
+
+
+    }
+}
